ee83d938b0ad44102eed1f8d9888ff73c5e54cc8
Patrick Meyers <meyers@Patricks-Mini.zyxel.com>
2016-11-26 17:03:45 -0600

    update travis yml for coveralls...hopefully...

---
+++ basketball_db/version.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) Duncan Macleod (2013)
+
+"""Versioning record for CIS
+"""
+
+__author__ = 'Duncan Macleod <duncan.macleod@ligo.org>'
+__version__ = 'ab564a2ecbd32ef4ac3ecd5182606f36da8e5739'
+__date__ = '2016-11-26 22:54:55 +0000'
+
+git_id = 'ab564a2ecbd32ef4ac3ecd5182606f36da8e5739'
+git_branch = 'master'
+git_tag = None
+git_author = 'Patrick Meyers <meyers@Patricks-Mini.zyxel.com>'
+git_committer = 'Patrick Meyers <meyers@Patricks-Mini.zyxel.com>'
+git_status = 'UNCLEAN: Modified working tree'
