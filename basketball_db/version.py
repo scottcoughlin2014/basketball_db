e5436973e77021e5b2cc690d570ac40d406ad48f
Patrick Meyers <meyers@Patricks-Mini.zyxel.com>
2016-11-26 17:13:14 -0600

    update...fix typo in yml

--- basketball_db/version.py
+++ basketball_db/version.py
@@ -5,12 +5,12 @@
 """
 
 __author__ = 'Duncan Macleod <duncan.macleod@ligo.org>'
-__version__ = 'ab564a2ecbd32ef4ac3ecd5182606f36da8e5739'
-__date__ = '2016-11-26 22:54:55 +0000'
+__version__ = 'ee83d938b0ad44102eed1f8d9888ff73c5e54cc8'
+__date__ = '2016-11-26 23:03:45 +0000'
 
-git_id = 'ab564a2ecbd32ef4ac3ecd5182606f36da8e5739'
+git_id = 'ee83d938b0ad44102eed1f8d9888ff73c5e54cc8'
 git_branch = 'master'
 git_tag = None
 git_author = 'Patrick Meyers <meyers@Patricks-Mini.zyxel.com>'
 git_committer = 'Patrick Meyers <meyers@Patricks-Mini.zyxel.com>'
-git_status = 'UNCLEAN: Modified working tree'
+git_status = None
