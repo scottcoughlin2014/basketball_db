c4caca52eb21bec96a2e16d51cb9df64cbf12f3c
Patrick Meyers <meyers@Patricks-Mac-mini.local>
2016-11-27 08:41:03 -0600

    add query database module

--- basketball_db/tests/create_db_test.py
+++ basketball_db/tests/create_db_test.py
@@ -1,17 +1,27 @@
 import unittest
 import numpy.testing as npt
 from ..create_db.core import save_team_season
+from ..query_db.core import query_db
 import os
 import shutil
 
 TEAM = 'MIN'
 YEAR = 2016
-fname = 'tests/data/2015/10/28/MIN-AT-LAL.hdf5'
+fname = 'TEST_DATA/2015/10/28/MIN-AT-LAL.hdf5'
+DATE1 = '20151027'
+DATE2 = '20151028'
 class TestCreate_db(unittest.TestCase):
     def test_create_db(self):
-        save_team_season(TEAM,YEAR, basedir='tests/data', games=[1])
+        save_team_season(TEAM,YEAR, basedir='TEST_DATA', games=[1])
         self.assertTrue(os.path.isfile(fname))
-        shutil.rmtree('tests/data/2015')
+    def test_query_db(self):
+        files = query_db(TEAM, date_start=DATE1, date_end=DATE2,
+            basedir='TEST_DATA')
+        self.assertTrue(files[0]==fname)
+    def clean_up(self):
+        shutil.rmtree('TEST_DATA')
+        print 'Cleaned'
+        self.assertTrue(True)
 
 if __name__=="__main__":
     unittest.main()
--- basketball_db/version.py
+++ basketball_db/version.py
@@ -5,10 +5,10 @@
 """
 
 __author__ = 'Duncan Macleod <duncan.macleod@ligo.org>'
-__version__ = 'ee83d938b0ad44102eed1f8d9888ff73c5e54cc8'
-__date__ = '2016-11-26 23:03:45 +0000'
+__version__ = 'dfbee8be6435d895adacd885b2a1623f603cec55'
+__date__ = '2016-11-27 00:41:39 +0000'
 
-git_id = 'ee83d938b0ad44102eed1f8d9888ff73c5e54cc8'
+git_id = 'dfbee8be6435d895adacd885b2a1623f603cec55'
 git_branch = 'master'
 git_tag = None
 git_author = 'Patrick Meyers <meyers@Patricks-Mini.zyxel.com>'
