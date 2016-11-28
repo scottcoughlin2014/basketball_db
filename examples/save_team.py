f70e2c74cfe9b358efdf47c45d1c6cb7d928e53f
scottcoughlin2014 <scottcoughlin2014@u.northwestern.edu>
2016-11-28 11:14:37 -0600

    Making some values changeable by an ini file that would be obnoxious for the command line

--- examples/save_team.py
+++ examples/save_team.py
@@ -8,8 +8,10 @@ from basketball_db import create_db, query_db
 from basketball_db import utils
 from court import bball_court_half, bball_court_three
 
-import numpy as np
 import argparse
+import ConfigParser
+import json
+import numpy as np
 import pandas as pd
 
 def parse_commandline():
@@ -20,6 +22,7 @@ def parse_commandline():
     parser.add_argument("--team", help="What team do you want to make plots for. Did you query all the appropriate data for this team before running this?                     [ATL,BOS,BRK,CHI,CHO,CLE,DAL,DEN,DET,GSW,HOU,IND,LAC,LAL,MEM,MIA,MIL,MIN,NOP,NYK,OKC,ORL,PHI,PHO,POR,SAC,SAS,TOR,UTA,WAS]")
     parser.add_argument("--YMDStart", help="[Format: 20151028] What date would you like to start aggregating data for.")
     parser.add_argument("--YMDEnd", help="[Format: 20160413] What date would you like to end aggregating data for.")
+    parser.add_argument("--inifile", help="Name of ini file to deal with nitty gritty stuff that is annoying to specify at the command line")
 
     args = parser.parse_args()
 
@@ -28,6 +31,15 @@ def parse_commandline():
 args = parse_commandline()
 # need 3-letter code for team...Will add a list of these
 TEAM = args.team
+# ---- Create configuration-file-parser object and read parameters file.
+cp = ConfigParser.ConfigParser()
+cp.read(args.inifile)
+figSizeShot = json.loads(cp.get('plotsShot','figSize'))
+figNameShot = cp.get('plotsShot','figName')
+figSizeHeatShot = json.loads(cp.get('plotsHeatShot','figSize'))
+figNameHeatShot = cp.get('plotsHeatShot','figName')
+figSizeHeatPoint = json.loads(cp.get('plotsHeatPoint','figSize'))
+figNameHeatPoint = cp.get('plotsHeatPoint','figName')
 # run the code
 #create_db.save_team_season(TEAM, YEAR, games=GAMES)
 #create_db.save_team_season(TEAM, YEAR)
@@ -58,13 +70,13 @@ for ii,filename in enumerate(files):
 shots = pd.concat(shots)
 
 # plot the shot chart
-plt.figure(figsize=(5,4.7))
+plt.figure(figsize=figSizeShot)
 ax = bball_court_half()
 ax.scatter(50- shots['shot_xs'][shots['result']],
     shots['shot_ys'][shots['result']],c='g' )
 ax.scatter(50- shots['shot_xs'][~shots['result']],
     shots['shot_ys'][~shots['result']],c='r' )
-plt.savefig('test')
+plt.savefig(figNameShot)
 plt.close()
 
 xedges = np.arange(50)
@@ -86,13 +98,13 @@ H_no_perc[np.isnan(H_no_perc)] = 0.0
 print "Percentage of shots made: %.5f%%"%(np.nansum(H_yes)/np.nansum(H_total))
 
 # heatmap for the shot chart
-plt.figure(figsize=(5.3,4.7))
+plt.figure(figsize=figSizeHeatShot)
 ax = bball_court_half()
 img = ax.pcolor(X,Y,H_yes_perc, cmap='RdBu_r', vmin=0,vmax=1)
 divider = make_axes_locatable(ax)
 cax = divider.append_axes("right", size="5.6%", pad=0.05)
 plt.colorbar(img,label='Shot percentage',cax=cax)
-plt.savefig('hist2d')
+plt.savefig(figNameHeatShot)
 plt.close()
 
 norm_arr = (H_yes_perc*points).reshape((points.size,1)).squeeze()
@@ -102,12 +114,12 @@ print norm
 print std
 
 # heat map based on points
-plt.figure(figsize=(5.3,4.7))
+plt.figure(figsize=figSizeHeatPoint)
 ax = bball_court_half()
 img = ax.pcolor(X,Y,H_yes_perc*points, cmap='Spectral_r', vmin=norm-0.5,
         vmax=norm+0.5)
 divider = make_axes_locatable(ax)
 cax = divider.append_axes("right", size="5.6%", pad=0.05)
 plt.colorbar(img,label='Expected Points',cax=cax)
-plt.savefig('points')
+plt.savefig(figNameHeatPoint)
 plt.close()
